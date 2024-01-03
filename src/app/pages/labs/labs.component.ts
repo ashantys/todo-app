58330fc30d809f36715babc9be067228b7542922
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-01-03 14:43:09 -0600

    feat(signal): signals practice

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -18,7 +18,7 @@ export class LabsComponent {
     'Create project',
     'Create components'
   ];
-  name = 'Ashanty';
+  name = signal('Ashanty');
   age = 20;
 
 //Property Binding
@@ -37,7 +37,9 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event){
-    console.log(event);
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
   }
 
   keydownHandler(event: KeyboardEvent){
